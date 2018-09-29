fecfe7e16a60473aac3457cdb18982eefa4a1379
Michael <michael.kamel.2020@gmail.com>
2018-09-29 02:36:54 +0200

    add priority queue implementation for possible usage in UCS #1

---
+++ src/searching/algorithms/MinHeap.java
@@ -0,0 +1,112 @@
+package searching.algorithms;
+
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
+public class MinHeap<T extends Comparable<T>> extends PriorityQueue<T> {
+	private static final long serialVersionUID = 1L;
+	private ArrayList<T> heap;
+		
+	public MinHeap() {
+		this.heap = new ArrayList<T>();
+	}
+	public MinHeap(T node) {
+		this();
+		heap.add(node);
+	}
+	
+	public T peek() {
+		return heap.get(0);
+	}
+	
+	public T poll() {
+		if(heap.size() == 0)
+			return null;
+		
+		if(heap.size() == 1)
+			return heap.remove(0);
+		
+		T root = heap.get(0);
+		T newRoot = heap.remove(heap.size()-1);
+		heap.set(0, newRoot);
+		heapifyDown(0);
+		
+		return root;
+	}
+	
+	public boolean add(T value) {
+		heap.add(value);
+		heapifyUp(heap.size()-1);
+		return true;
+	}
+	
+	public void clear() {
+		heap.clear();
+	}
+	
+	public boolean contains(T value) {
+		return heap.contains(value);
+	}
+	
+	public boolean isEmpty() {
+		return heap.size() > 0;
+	}
+	
+	private void heapifyDown(int nodeIdx) {
+		T node = heap.get(nodeIdx);
+		T largest = node;
+		
+		int leftNodeIdx = nodeIdx*2+1;
+		int rightNodeIdx = leftNodeIdx+1;
+		
+		T leftNode;
+		if(leftNodeIdx < heap.size())
+			leftNode = heap.get(leftNodeIdx);
+		else
+			leftNode = null;
+		
+		T rightNode;
+		if(rightNodeIdx < heap.size())
+			rightNode = heap.get(rightNodeIdx);
+		else
+			rightNode = null;
+		
+		if(leftNode != null && leftNode.compareTo(largest) > 0)
+			largest = leftNode;
+		
+		if(rightNode != null && rightNode.compareTo(largest) > 0)
+			largest = rightNode;
+		
+		if(largest == node)
+			return;
+		
+		if(largest == leftNode) {
+			swapNodes(nodeIdx, leftNodeIdx);
+			heapifyDown(leftNodeIdx);
+		} else {
+			swapNodes(nodeIdx, rightNodeIdx);
+			heapifyDown(rightNodeIdx);
+		}
+	}
+	
+	private void heapifyUp(int nodeIdx) {
+		if(nodeIdx == 0)
+			return;
+		
+		T node = heap.get(nodeIdx);
+		int parentNodeIdx = (nodeIdx - 1) / 2;
+		T parentNode = heap.get(parentNodeIdx);
+		
+		if(node.compareTo(parentNode) > 0) {
+			swapNodes(nodeIdx, parentNodeIdx);
+			heapifyUp(parentNodeIdx);
+		}
+	}
+	
+	private void swapNodes(int firstIdx, int secondIdx) {
+		T firstNode = heap.get(firstIdx);
+		heap.set(firstIdx, heap.get(secondIdx));
+		heap.set(secondIdx, firstNode);
+	}
+	
+}
