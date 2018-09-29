5d1d80c249d79c859eb3841f4e10bc2594ef25c5
Michael <michael.kamel.2020@gmail.com>
2018-09-29 18:53:48 +0200

    modified abstract problem structure #1

--- src/searching/problems/SearchProblem.java
+++ src/searching/problems/SearchProblem.java
@@ -6,19 +6,24 @@ import searching.strategies.SearchTreeNode;
 
 public abstract class SearchProblem {
 	private final GoalTest goalTest;
+	private final SearchState initialState;
 	private final Enumeration<SearchAction> possibleActions;
 	
-	public SearchProblem(GoalTest goalTest, Enumeration<SearchAction> possibleActions) {
+	public SearchProblem(GoalTest goalTest, Enumeration<SearchAction> possibleActions, SearchState initialState) {
 		this.goalTest = goalTest;
 		this.possibleActions = possibleActions;
+		this.initialState = initialState;
 	}
-	public abstract SearchState getInitialState();
+	
+	public SearchState getInitialState() {
+		return this.initialState;
+	}
+	
+	public GoalTest getGoalTest() {
+		return this.goalTest;
+	}
 
 	public abstract SearchTreeNode makeNode(SearchState initialState);
-
-	public GoalTest getGoalTest() {
-		return goalTest;
-	}
 	
 	public Enumeration<SearchAction> getPossibleActions() {
 		return possibleActions;
